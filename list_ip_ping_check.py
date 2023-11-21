b53e52a518a26308c1406608190933224c70bb74
anton.rusakov <anton.rusakov@itglobal.com>
2023-11-21 18:13:11 +0300

    correct_code_comments

--- list_ip_ping_check.py
+++ list_ip_ping_check.py
@@ -1,13 +1,13 @@
-import subprocess
+import subprocess # модуль запуска внешнего процесса (пинга)
 
-# Чтение списка IP адресов из файла
+# чтение списка IP адресов из файла list_ip.txt
 with open('list_ip.txt', 'r') as file:
     ip_addresses = file.readlines()
 
-# Пингуем каждый IP адрес
+# пинг каждого IP адреса
 with open('list_ip_check.txt', 'w') as output_file:
     for ip in ip_addresses:
-        ip = ip.strip()  # Удаляем лишние пробелы и символы переноса строки
+        ip = ip.strip()  # удаление лишних пробелов и символов переноса строки
         result = subprocess.run(['ping', '-c', '3', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode == 0:
             output_file.write(f"{ip} available\n")
